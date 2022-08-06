3c0a27bc1db128a645832e4757e5c4eaa467e9be
StivenM <stimomez@gmail.com>
2022-08-06 17:31:57 -0500

    Agregando nuevas tecnologias, modificando estilos

--- logic/main.js
+++ logic/main.js
@@ -1,7 +1,36 @@
 const sections = document.querySelectorAll("section");
 const navList = document.querySelectorAll("nav div ul li");
 const checkbox = document.getElementById("menu-hamburguesa");
-//const style = document.documentElement.style;
+const rootStyles = document.documentElement.style;
+const formDesktop = document.getElementById("form-desktop");
+const formMobile = document.getElementById("form-mobile");
+const textAreaMobile = document.getElementById("text-area-mobile");
+const iconLoandingMobile = document.getElementById("icon-loanding-mobile");
+const textAreaDesktop = document.getElementById("text-area-desktop");
+const iconLoandingDesktop = document.getElementById("icon-loanding-desktop");
+
+let form = null;
+let textArea = null;
+let iconLoanding = null;
+
+if (screen.width >= 800) {
+  form = formDesktop;
+  textArea = textAreaDesktop;
+  iconLoanding = iconLoandingDesktop;
+} else {
+  form = formMobile;
+  textArea = textAreaMobile;
+  iconLoanding = iconLoandingMobile;
+}
+
+form = addEventListener("submit", () => isLoanding());
+
+const isLoanding = () => {
+  textArea.style.opacity = 0;
+  iconLoanding.style.opacity = 1;
+ 
+ 
+};
 
 navList.forEach((li) => {
   li.addEventListener("click", () => {
@@ -18,16 +47,15 @@ window.addEventListener("scroll", () => {
     const sectionHeigth = section.clientHeight;
 
     if (pageYOffset >= sectionTop - sectionHeigth / 3) {
-      
       section.classList.add("transitions");
       current = section.getAttribute("id");
+      
     }
   });
   navList.forEach((li) => {
     li.classList.remove("active");
     if (li.classList.contains(current)) {
       li.classList.add("active");
-      
     }
   });
 });
